4fa6fe00a93de1c9f2b7a17820e770e21c670aae
Zsolt Nagy <info@zsoltnagy.eu>
2023-03-09 02:23:36 +0100

    Add item

--- src/App.js
+++ src/App.js
@@ -19,10 +19,26 @@ function App() {
     }
   ]);
 
+  function addFeatureItem(feature, version, year) {
+    setFeatureItems(oldFeatureItems => {
+      // Step 1: deep clone
+      const newFeatureItems = structuredClone(oldFeatureItems);
+      // Step 2: assemble the new feature items value 
+      newFeatureItems.push({
+        feature,
+        version,
+        year, 
+        id: new Date().getTime()
+      });
+      // Step 3: return the assembled new value
+      return newFeatureItems;
+    });
+  }
+
   return (
     <div className="App">
       <h1>JavaScript Features App</h1>
-      <FeaturesForm />
+      <FeaturesForm addFeatureItem={addFeatureItem} />
       <FeaturesList featureItems={featureItems} />
     </div>
   );
--- src/Components/FeaturesForm/FeaturesForm.jsx
+++ src/Components/FeaturesForm/FeaturesForm.jsx
@@ -1,7 +1,66 @@
 import React from 'react';
 
-export default function FeaturesForm() {
+export default function FeaturesForm(props) {
+    const [feature, setFeature] = React.useState('');
+    const [version, setVersion] = React.useState('');
+    const [year, setYear] = React.useState('');
+
+    function handleFeatureChange(event) {
+        setFeature(event.target.value);
+    }
+
+    function handleVersionChange(event) {
+        setVersion(event.target.value);
+    }
+
+    function handleYearChange(event) {
+        setYear(event.target.value);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        props.addFeatureItem(feature, version, year);
+        setFeature('');
+        setVersion('');
+        setYear('');
+    }
+
     return (
-        <div>Features Form Component</div>
+        <form action="#" method="GET" onSubmit={handleSubmit}>
+            <div>
+                <label>
+                    Feature:
+                    <input 
+                        type="text" 
+                        name="feature" 
+                        value={feature} 
+                        onChange={handleFeatureChange} />
+                </label>
+            </div>
+            <div>
+                <label>
+                    Version:
+                    <input 
+                        type="text" 
+                        name="version" 
+                        value={version}
+                        onChange={handleVersionChange} />
+                </label>
+            </div>
+            <div>
+                <label>
+                    Year:
+                    <input 
+                        type="number" 
+                        name="year" 
+                        value={year}
+                        onChange={handleYearChange} />
+                </label>
+            </div>
+            <div>
+                <button type="submit">Create</button>
+            </div>
+
+        </form>
     );
 }
