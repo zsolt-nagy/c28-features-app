e1b10f2361d72e0ef332d21280552941aabd5e2c
Zsolt Nagy <info@zsoltnagy.eu>
2023-03-11 17:54:41 +0100

    add item

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import FeaturesForm from './Components/FeaturesForm/FeaturesForm';
 import FeaturesList from './Components/FeaturesList/FeaturesList';
 
+const SERVER_URL = 'https://qxzje9-8080.csb.app';
+
 function App() {
   const [featureItems, setFeatureItems] = useState([]);
 
-  useEffect(() => {
+  const loadData = () => {
     axios
-      .get('https://qxzje9-8080.csb.app/api/features/')
+      .get(SERVER_URL + '/api/features/')
       .then((response) => {
         if (Array.isArray(response.data)) {
           setFeatureItems(response.data);
@@ -20,22 +22,19 @@ function App() {
       .catch((error) => {
         setFeatureItems([]);
       });
-  }, []);
+  }
+
+  useEffect(loadData, []);
 
   function addFeatureItem(feature, version, year) {
-    setFeatureItems(oldFeatureItems => {
-      // Step 1: deep clone
-      const newFeatureItems = structuredClone(oldFeatureItems);
-      // Step 2: assemble the new feature items value 
-      newFeatureItems.push({
-        feature,
-        version,
-        year, 
-        id: new Date().getTime()
-      });
-      // Step 3: return the assembled new value
-      return newFeatureItems;
-    });
+    if (feature === '' || version === '' || year === '') {
+      return;
+    }
+    axios.post(SERVER_URL + '/api/features/new', {
+      feature,
+      version,
+      year
+    }).then(loadData);
   }
 
   function deleteFeatureItem(id) {
